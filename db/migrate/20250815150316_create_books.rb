7e4c2059de123d86aa1a3990a8189c2b0a27740e
jabezinu <https://github.com/jabezinu>
2025-08-15 18:03:27 +0300

    book model

---
+++ db/migrate/20250815150316_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :isbn
+      t.references :category, null: false, foreign_key: true
+      t.boolean :available
+
+      t.timestamps
+    end
+  end
+end
