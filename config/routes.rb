8b5b56f97638a6b2cdae50fb097ef9154963cac3
jabezinu <https://github.com/jabezinu>
2025-08-15 18:06:51 +0300

    controller route view for books

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "books/index"
+  get "books/show"
+  get "books/new"
+  get "books/create"
+  get "books/edit"
+  get "books/update"
+  get "books/destroy"
   get "categories/index"
   get "categories/show"
   get "categories/new"
